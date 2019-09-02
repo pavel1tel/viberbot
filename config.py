8c7efa2bd8cd064e85e7d23d4abf1402c8b5e06b
Pavel Ivaschenko <pawloiwanov@gmail.com>
2019-09-02 18:05:56 +0300

    fix bugs

--- config.py
+++ config.py
@@ -7,4 +7,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = 'pawloiwanov@gmail.com'
